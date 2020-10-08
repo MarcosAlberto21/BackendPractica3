974c36e635c0ef7481dd35db1cf5805f0fb4c494
pabloramirez11294 <pabloramirez.11294@gmail.com>
2020-10-08 16:04:16 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.get('/', (req,res) => {
+    res.send('API gifthub');
+});
+
+app.listen(PORT, () =>{
+    console.log(`Server running on port ${PORT}`);
+});
