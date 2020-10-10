ec3383654cf208c6f4414ac598e96f99be07369c
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2020-10-09 23:27:01 -0600

    Conectar a la base de datos mysql

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const app = express();
+const mysqlConnection  = require('./database.js');
 const PORT = process.env.PORT || 3000;
+
+
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
