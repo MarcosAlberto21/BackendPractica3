fe56acc584f2b5f09c34753a23d1ce3341a349b6
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2020-10-16 20:51:30 -0600

    Realizar prueba unitaria de conexion con la base de datos

--- test/test.js
+++ test/test.js
@@ -1,8 +1,39 @@
 var assert = require('assert');
+const mysqlConnection  = require('../src/database');
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.strictEqual([1, 2, 3].indexOf(4), -1);
     });
+  });
+});
+
+
+describe('Base de datos', function () {
+  it('Conexion con la base de datos exitosa', function () {
+    mysqlConnection.query('SELECT * FROM Usuario', (err, rows, fields) => {
+      if(!err) {
+        const usuario=rows;
+        assert.notStrictEqual(usuario,undefined);
+      } else {
+        console.log(err);
+      }
+    });  
+    assert.strictEqual([1, 2, 3].indexOf(4), -1);
+  });
+});
+
+describe('Login', function () {
+  it('Login exitoso', function () {
+    mysqlConnection.query('SELECT * FROM Usuario WHERE username = ? AND contrasenia=?'
+                          ,['user1','123'], (err, rows, fields) => {
+      if(!err) {
+        const usuario=rows[0];
+        assert.notStrictEqual(usuario,undefined);
+      } else {
+        console.log(err);
+      }
+    });  
+    assert.strictEqual([1, 2, 3].indexOf(4), -1);
   });
 });
