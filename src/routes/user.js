fc8f7074f6e6246f72c464c4967aa7c5406198c7
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2020-10-27 20:11:04 -0600

    Se hizo el endpoint actualizarUsuario

--- src/routes/user.js
+++ src/routes/user.js
@@ -14,6 +14,33 @@ router.get('/user',(req,res) => {
       });  
 });
 
+router.get('/user/:id',(req,res) => {
+  const { id }=req.params;
+  mysqlConnection.query('SELECT * FROM Usuario WHERE id_usuario=?',id, (err, rows, fields) => {
+      if(!err) {
+        res.status(200).json(rows);
+      } else {
+        console.log(err);
+        res.status(409).send({ message: 'Problema al solicitar usuarios.' });
+      }
+    });  
+});
+router.put('/actualizarUsuario/:id',(req,res) => {
+  const { id }=req.params;
+  const val = [req.body.username,req.body.correo,req.body.contrasenia,req.body.nombre,req.body.apellido,req.body.dpi,req.body.edad,id];
+  
+  console.log('*****',val);
+  mysqlConnection.query("UPDATE Usuario set username=?, correo=?, contrasenia=?,"+
+                        "nombre=?, apellido=?, dpi=?, edad=? WHERE id_usuario=?",val, (err, rows, fields) => {
+      if(!err) {
+        res.status(200).json(rows);
+      } else {
+        console.log(err);
+        res.status(409).send({ message: 'Problema al actualizar usuario.' });
+      }
+    });  
+});
+
 
 //ingresar usuario
 router.post('/user', (req, res) => {
