fa1f31fc48af5bf364f866e89f8f5dada2c0839f
Pablo David Ramírez Fernández <pabloramirez.11294@gmail.com>
2020-11-02 14:38:34 -0600

    Pruebas unitarias login y registro

--- test/test.js
+++ test/test.js
@@ -1,5 +1,13 @@
 var assert = require('assert');
 const mysqlConnection  = require('../src/database');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+
+chai.use(chaiHttp);
+const url= 'http://localhost:3000';
+
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
@@ -23,23 +31,84 @@ describe('Base de datos', function () {
   });
 });
 
-
+//login
 describe('Login', function () {
   it('Login exitoso', function () {
-    mysqlConnection.query('SELECT * FROM Usuario WHERE username = ? AND contrasenia=?'
-                          ,['user1','123'], (err, rows, fields) => {
-      if(!err) {
-        const usuario=rows[0];
-        assert.notStrictEqual(usuario,undefined);
-      } else {
-        console.log(err);
-      }
-    });  
-    assert.strictEqual([1, 2, 3].indexOf(4), -1);
+    chai.request(url)
+      .post('/login')
+      .send({ username:'user2', contrasenia: '123' })
+      .end( function(err,res){
+         
+          if(err){
+            console.log(err);
+          }else{
+            console.log(res.body)
+            expect(res).to.have.status(200);
+            //done();
+          }
+           
+        });
+  });
+
+  it('Login fallido', function () {
+    chai.request(url)
+      .post('/login')
+      .send({ username:'userIncorrecto', contrasenia: '123' })
+      .end( function(err,res){
+         
+          if(err){
+            console.log(err);
+          }else{
+            console.log(res.body)
+            expect(res).to.have.status(409);
+            //done();
+          }
+           
+      });
   });
 });
 
+//registro
+describe('Registro', function () {
+  it('Registro exitoso', function () {
+    const random = Math.floor(Math.random() * (10000 - 200)) + 200;
+    chai.request(url)
+      .post('/user')
+      .send({  username:`user${random}`,correo:`user${random}@gmail.com`, contrasenia:`user${random}`,nombre:`user${random}`,apellido:`user${random}`,dpi:`${random}`,edad:`user${random}`,id_tipousuario:'1'})
+      .end( function(err,res){
+         
+          if(err){
+            console.log(err);
+          }else{
+            console.log(res.body)
+            expect(res).to.have.status(200);
+            //done();
+          }
+           
+        });
+  });
+  it('Registro fallido', function () {
+    chai.request(url)
+      .post('/user')
+      .send({  })
+      .end( function(err,res){
+         
+          if(err){
+            console.log(err);
+          }else{
+            console.log(res.body)
+            expect(res).to.have.status(409);
+            //done();
+          }
+           
+        });
+  });
+  
+});
 
+
+
+/*
 describe('giftcard', function () {
   it('historial', function () {
     mysqlConnection.query('SELECT * FROM Usuario WHERE username = ? AND contrasenia=?'
@@ -53,4 +122,4 @@ describe('giftcard', function () {
     });  
     assert.strictEqual([1, 2, 3].indexOf(4), -1);
   });
-});
+});*/
